f96074ecea1751c8b2905b72968c9f4f3a4b99fa
KEYFSLTD\Laurie.Bassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-09-30 16:34:12 +0100

    initial commit

---
+++ TaskManagerApp/TaskManagerApp.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerApp
+{
+    class TaskManagerApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Task Manager App ---");
+        }
+    }
+}
